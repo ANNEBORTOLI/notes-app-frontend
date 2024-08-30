393a348b965b79bbed986bf8daadef5df07cd5ad
Anne Bortoli <annebortoli@gmail.com>
2024-08-30 11:48:07 -0300

    tag component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return (
@@ -20,7 +21,10 @@ export function Details() {
         </Links>
       </Section>
 
-      <Section title="Marcadores"></Section>
+      <Section title="Marcadores">
+        <Tag title="Express" />
+        <Tag title="Node" />
+      </Section>
 
       <Button title="Voltar" />
     </Container>
