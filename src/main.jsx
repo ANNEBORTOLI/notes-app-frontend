0e4787c97bb653b45269540a538b39da74a4dd80
Anne Bortoli <annebortoli@gmail.com>
2024-09-02 12:27:11 -0300

    Profile page

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,14 @@ import theme from "./styles/theme";
 // import { Details } from "./pages/Details";
 // import { Home } from "./pages/Home";
 // import { SignIn } from "./pages/SignUp";
-import { SignUp } from "./pages/SignUp";
+// import { SignUp } from "./pages/SignUp";
+import { Profile } from "./pages/Profile";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </StrictMode>
 );
